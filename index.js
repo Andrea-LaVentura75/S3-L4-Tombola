a2e8ce31f7264277c8d5683035b7950848033a3c
Andrea La Ventura <laventuraandrea40@gmail.com>
2024-08-29 21:04:51 +0200

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let corpoTombola = document.querySelector(".tombola");
 let celleNumeri = [];
-console.log(celleNumeri);
+let arrayDiv = [];
+console.log(celleNumeri, arrayDiv);
+
 const stampareNumeriSchermo = function () {
   for (let i = 0; i < 90; i++) {
     let valoreNumerico = i + 1;
@@ -14,32 +16,24 @@ const stampareNumeriSchermo = function () {
     contenitoreNumero.appendChild(numeriTombola);
     corpoTombola.appendChild(contenitoreNumero);
 
-    celleNumeri.push({ numero: valoreNumerico, elemento: contenitoreNumero });
+    celleNumeri.push(valoreNumerico);
+    arrayDiv.push(contenitoreNumero);
   }
 };
+
 let numeriUsciti = [];
-console.log(numeriUsciti);
-stampareNumeriSchermo();
-let button = document.querySelector(".estrai");
-button.addEventListener("click", function () {
-  let numeroCasuale = Math.floor(Math.random() * 90) + 1;
-  console.log(numeroCasuale);
-  for (let i = 0; i < celleNumeri.length; i++) {
-    if (numeroCasuale === celleNumeri[i].numero) {
-      numeriUsciti.push(numeroCasuale);
-      if (numeroCasuale !== numeriUsciti) {
-        numeriUsciti.push([numeroCasuale]);
-        celleNumeri[i].elemento.style.backgroundColor = "#ffe200";
-      } else {
-        Math.floor(Math.random() * 90) + 1;
-      }
-    }
-  }
-});
 let miniTabella = document.querySelector(".mini-tabella");
+let miniTabellaNumeri = [];
+let miniTabellaDivs = [];
+
 const stampaMiniTabella = function () {
   for (let i = 0; i < 24; i++) {
     let celleCasuali = Math.floor(Math.random() * 90) + 1;
+
+    while (miniTabellaNumeri.includes(celleCasuali)) {
+      celleCasuali = Math.floor(Math.random() * 90) + 1;
+    }
+
     let contenitoreNumero = document.createElement("div");
     contenitoreNumero.classList.add("conteiner-numero");
 
@@ -48,6 +42,36 @@ const stampaMiniTabella = function () {
 
     contenitoreNumero.appendChild(numeriMiniTabella);
     miniTabella.appendChild(contenitoreNumero);
+
+    miniTabellaNumeri.push(celleCasuali);
+    miniTabellaDivs.push(contenitoreNumero);
   }
 };
+
+stampareNumeriSchermo();
 stampaMiniTabella();
+
+let button = document.querySelector(".estrai");
+button.addEventListener("click", function () {
+  if (celleNumeri.length > 0) {
+    let indiceCasuale = Math.floor(Math.random() * celleNumeri.length);
+    let numeroCasuale = celleNumeri[indiceCasuale];
+    console.log(numeroCasuale);
+    numeriUsciti.push(numeroCasuale);
+
+    arrayDiv[numeroCasuale - 1].style.backgroundColor = "#ffe200";
+    celleNumeri.splice(indiceCasuale, 1);
+
+    let indexInMiniTabella = miniTabellaNumeri.indexOf(numeroCasuale);
+    if (indexInMiniTabella !== -1) {
+      miniTabellaDivs[indexInMiniTabella].style.backgroundColor = "#ffe200";
+    }
+
+    let contenitoreContatore = document.querySelector(".ultimo-numero p");
+    contenitoreContatore.innerText = `L'ultimo numero pescato è: ${numeroCasuale}`;
+    let contatore = document.createElement("p");
+    contenitoreContatore.appendChild(contatore);
+  } else {
+    alert("Mi dispiace i numeri sono terminati");
+  }
+});
